3c7ba0954e6dac928036dda350bbe0c32b88e887
AlexBCIT <asomerville1@my.bcit.ca>
2023-03-31 23:41:48 -0700

    finishing debug

--- .next/static/webpack/webpack.05ad74c1b2de3b05.hot-update.js
+++ .next/static/webpack/webpack.05ad74c1b2de3b05.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "643037148d8bbf1b"; }
+/******/ 	__webpack_require__.h = function() { return "3d9055ea7cdf7fe7"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
--- pages/api/create.ts
+++ pages/api/create.ts
@@ -3,37 +3,46 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { getServerSession } from "next-auth";
 import { authOptions } from "./auth/[...nextauth]";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse){
-    const {title, content} = req.body
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    const { title, content } = req.body
 
     const session = await getServerSession(req, res, authOptions)
-    
+
     if (!session) {
         res.status(401).json({ error: 'Unauthorized' })
         return
     }
 
+    // const prismaUser = await prisma.user.findUnique({
+    //     where: { email: session.user.email },
+    // })
+
+    const userEmail = session.user?.email; // use optional chaining to ensure that userEmail is not null or undefined
+    if (!userEmail) {
+        // handle the case where userEmail is null or undefined, for example by throwing an error or redirecting to a login page
+    }
     const prismaUser = await prisma.user.findUnique({
-        where: { email: session.user.email },
-    })
+        where: { email: userEmail },
+    });
+
 
     console.log(prismaUser)
 
     if (!prismaUser) {
-        res.status(401).json({ error: 'Unauthorized'})
+        res.status(401).json({ error: 'Unauthorized' })
         return
     }
 
     try {
         await prisma.note.create({
             data: {
-                title, 
+                title,
                 content,
                 userId: prismaUser.id
             }
         })
-        res.status(200).json({message: 'Note Created!'})
-    } catch (error){
+        res.status(200).json({ message: 'Note Created!' })
+    } catch (error) {
         console.log(error)
     }
 }
